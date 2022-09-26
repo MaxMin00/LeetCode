358c6cbe08f1b1220ccc74f5d62285c77a466670
MaxMin00 <102911137+MaxMin00@users.noreply.github.com>
2022-09-26 17:58:12 -0500

    Time: 6720 ms (5.00%), Space: 15.5 MB (14.94%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -2,6 +2,8 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
         myList = list(range(n+1))
+        print(myList)
         for element in myList :
+            print('.')
             if not element in nums :
                 return element
